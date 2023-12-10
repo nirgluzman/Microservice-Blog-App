ef6da775a86d234bf48ff1a206aac9554367df24
nirgluzman <nir.gluzman@gmail.com>
2023-12-10 21:35:15 +0100

    create CommentCreate component

---
+++ client/src/components/PostList.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import CommentCreate from './CommentCreate.js';
+
+const PostList = () => {
+	const [posts, setPosts] = useState({});
+
+	const fetchPosts = async () => {
+		const res = await axios.get('http://localhost:4000/posts');
+		setPosts(res.data);
+	};
+
+	useEffect(() => {
+		fetchPosts();
+	}, []);
+
+	const renderedPosts = Object.values(posts) // array of posts
+		.map(post => {
+			return (
+				<div className='card' style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
+					<div className='card-body'>
+						<h3>{post.title}</h3>
+						<CommentCreate postId={post.id} />
+					</div>
+				</div>
+			);
+		});
+
+	return <div className='d-flex flex-row flex-wrap justify-content-between '>{renderedPosts}</div>;
+};
+
+export default PostList;
