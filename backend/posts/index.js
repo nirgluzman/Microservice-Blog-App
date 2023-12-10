3c9a301993169cab7d4619f7d283b19232193ef3
nirgluzman <nir.gluzman@gmail.com>
2023-12-10 16:54:05 +0100

    create posts service

---
+++ backend/posts/index.js
@@ -0,0 +1,26 @@
+import { randomBytes } from 'crypto';
+import express from 'express';
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// parse incoming Request Object as a JSON Object
+app.use(express.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+	res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
+
+	posts[id] = { id, title };
+
+	res.status(201).send(posts[id]);
+});
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
