1a6bdf96356a87571f3bfe877e3f56c441c7997b
nirgluzman <nir.gluzman@gmail.com>
2023-12-11 01:25:18 +0100

    update ports

--- backend/event-bus/index.js
+++ backend/event-bus/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -15,8 +15,8 @@ app.post('/events', async (req, res) => {
 	console.log('Event Received:', event.type); // log the event type
 
 	await axios.post('http://localhost:4000/events', event); // send the event to 'posts'
-	await axios.post('http://localhost:5000/events', event); // send the event to 'comments'
-	await axios.post('http://localhost:6000/events', event); // send the event to 'query'
+	await axios.post('http://localhost:4100/events', event); // send the event to 'comments'
+	await axios.post('http://localhost:4200/events', event); // send the event to 'query'
 
 	res.send({ status: 'OK' });
 });
